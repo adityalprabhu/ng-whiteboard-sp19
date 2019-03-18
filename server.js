881060bb9c3134aa368a28d0f2042f80998e3cc0
Aditya Prabhu <adityalprabhu@gmail.com>
2019-03-18 14:56:14 -0400

    new build

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+// const express = require('express');
+// const app = express();
+// app.use(express.static('./dist'));
+// app.get('/*', function (req, res) {
+//   res.sendFile(__dirname + '/dist/index.html');
+// });
+// app.listen(process.env.PORT || 8080);
+
+
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static('./dist'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/ng-sp19-adityalprabhu'));
+
 app.get('/*', function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(path.join('./dist/ng-sp19-adityalprabhu/index.html'));
 });
 app.listen(process.env.PORT || 8080);
